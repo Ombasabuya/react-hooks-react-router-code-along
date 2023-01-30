fc217f189a34744101926d12513dc397320cd208
ombasaabuya <ombasabuya@gmail.com>
2023-01-30 10:12:30 +0300

    modified index code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// Step 1. Import react-router functions
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-// Step 2. Change so router is coordinating what is displayed
 ReactDOM.render(
   <BrowserRouter>
-    <Route path="/">
-      <Home />
-    </Route>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
