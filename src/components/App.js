c87776fe93d8e7cd7d6a233b70a81d1eda07c9dc
ombasaabuya <ombasabuya@gmail.com>
2023-01-30 13:30:22 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,32 @@ import Home from "./Home";
 import About from "./About";
 import Login from "./Login";
 import NavBar from "./NavBar";
+import Signup from "./Signup";
+import Messages from "./Messages";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Switch>
-        <Route exact path="/about">
-          <About />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/messages">
+            <Messages />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+  
+  export default App;
